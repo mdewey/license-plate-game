bca0d232d342e0b8b5d18f5d4e6b0198406b200f
Mark Dewey <mark.t.dewey@gmail.com>
2018-06-17 21:37:58 -0400

    port

--- server/index.js
+++ server/index.js
@@ -52,6 +52,5 @@ app.use('/api', graphqlHTTP({rootValue, schema, graphiql: true}))
 app.use('/', express.static('dist'))
 
 const port = process.env.PORT || 4000;
-app.listen(port);
 
 app.listen(port, () => console.log('Listening on ' + port)) // eslint-disable-line no-console
